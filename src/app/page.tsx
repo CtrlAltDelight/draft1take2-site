4984ea034b50e46982920a89ffb3c0fdc5d25d86
CtrlAltDelight <lbchigges@gmail.com>
2023-11-14 17:42:44 -0500

    Changed to Callie's full name and changed email

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default async function Home() {
 		<div className="bg-black">
 			<div className="bg-brown md:w-5/6 w-auto m-auto">
 				<header className='bg-maroon text-tan h-32 flex rounded-b-3xl'>
-					<h2 className='font-serif ml-5 self-center text-6xl'>Draft 1, Take 2</h2>
+					<h2 className='font-serif ml-2 md:ml-5 self-center text-6xl'>Draft 1, Take 2</h2>
 				</header>
 				<main className="flex text-orange items-center min-h-screen flex-col pt-20 gap-y-14">
 					<Image
@@ -32,12 +32,12 @@ export default async function Home() {
 						<h2 className="text-center text-2xl font-bold">Meet the hosts!</h2>
 						<div className='flex justify-around'>
 							<div className='flex flex-col items-center'>
-								<h3 className='text-xl'>Callie</h3>
+								<h3 className='text-xl'>Callie J. Waligora</h3>
 								<p>A Hollywood script supervisor.</p>
 							</div>
 							<div className='flex flex-col items-center'>
-								<h3 className='text-xl'>Luke</h3>
-								<p>A web designer...</p>
+								<h3 className='text-xl'>Luke Chigges</h3>
+								<p>A craft beer enthusiast and movie enjoyer.</p>
 							</div>
 						</div>
 					</section>
@@ -51,7 +51,7 @@ export default async function Home() {
 					</section>
 				</main>
 				<footer className="p-4 bg-maroon rounded-t-2xl">
-					<p className='font-serif'>Questions? Email us at pod@chigges.com</p>
+					<p className='font-serif'>Questions? Email us at pod@draft1take2.com</p>
 				</footer>
 			</div>
 		</div>
